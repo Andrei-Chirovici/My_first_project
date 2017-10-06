507da676e0f9eee9906d42510c9ecbbd1e911542
61494 <achirovici.61494@farnborough.ac.uk>
2017-10-06 16:13:16 +0100

    Version 1.1

--- src/Main.java
+++ src/Main.java
@@ -11,12 +11,11 @@ import javafx.event.*;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
-
 import javax.swing.text.StyledEditorKit;
 
 public class Main {
 
-    public static void main(String[] args) {
+public static void main(String[] args) {
     launchFX();
 
     }
@@ -32,34 +31,21 @@ public class Main {
     private static void initialiseGUI()
     {
         Stage stage = new Stage();
+
         Pane rootPane = new Pane();
-        stage.setScene(new Scene(rootPane));
+        Scene scene = new Scene(rootPane);
+        scene.getStylesheets().add("style.css");
+
+        stage.setScene(scene);
         stage.show();
 
+        // Label //
         Label label = new Label();
         label.setText("Welcome to the Login Portal");
-        label.setId(fancytext);
+        label.setId("fancytext");
+        rootPane.getChildren().add(label);
 
-        #fancytext {
-        -fx-font: 100px Tahoma;
-        -fx-fill: linear-gradient(from 0% 0% to 100% 200%, repeat, aqua 0%, red 50%);
-        -fx-stroke: black;
-        -fx-stroke-width: 1;
-    }
-            rootPane.getChilden().add(label);
-
-
-        // Button Settings
-        Button btn = new Button();
-        btn.setText("Enter");
-        btn.setLayoutX(170);
-        btn.setLayoutY(250);
-        btn.setPrefWidth(60);
-        btn.setPrefHeight(30);
-        btn.setOnAction((ActionEvent ae) -> printHello());
-        rootPane.getChildren().add(btn);
-
-        // Username TextField Settings
+        // Username TextField Settings //
         TextField txtfield = new TextField();
         txtfield.setLayoutX(170);
         txtfield.setLayoutY(150);
@@ -76,9 +62,86 @@ public class Main {
         passfield.setPrefHeight(30);
         passfield.setPromptText("Password");
         rootPane.getChildren().add(passfield);
+
+        // Sign-Up Button Settings //
+        Button btn1 = new Button();
+        btn1.setText("Sign-Up");
+        btn1.setLayoutX(170);
+        btn1.setLayoutY(350);
+        btn1.setPrefWidth(60);
+        btn1.setPrefHeight(30);
+        btn1.setOnAction((ActionEvent ae) -> signUp());
+        rootPane.getChildren().add(btn1);
+
+        // Sign-Up Button Settings //
+        Button btn2 = new Button();
+        btn2.setText("Login");
+        btn2.setLayoutX(170);
+        btn2.setLayoutY(300);
+        btn2.setPrefWidth(60);
+        btn2.setPrefHeight(30);
+        btn2.setOnAction((ActionEvent ae) -> mainScreen());
+        rootPane.getChildren().add(btn2);
     }
 
-    private static void printHello() {
+    private static void mainScreen() {
+ //Firstly needs to check whether credentials are correct
+
+    }
+
+    private static void signUp() {
+        new JFXPanel();
+
+    Stage stage = new Stage();
+
+        Pane rootPane = new Pane();
+        Scene scene = new Scene(rootPane);
+        scene.getStylesheets().add("style.css");
+
+        stage.setScene(scene);
+        stage.show();
+
+        // Username TextField Settings //
+        TextField txtfield = new TextField();
+        txtfield.setLayoutX(170);
+        txtfield.setLayoutY(150);
+        txtfield.setPrefWidth(300);
+        txtfield.setPrefHeight(30);
+        txtfield.setPromptText("Username");
+        rootPane.getChildren().add(txtfield);
+
+        // Password Box Settings
+        PasswordField passfield = new PasswordField();
+        passfield.setLayoutX(170);
+        passfield.setLayoutY(200);
+        passfield.setPrefWidth(300);
+        passfield.setPrefHeight(30);
+        passfield.setPromptText("Password");
+        rootPane.getChildren().add(passfield);
+
+        // Password Confirm-Box Settings
+        PasswordField passfield1 = new PasswordField();
+        passfield.setLayoutX(170);
+        passfield.setLayoutY(250);
+        passfield.setPrefWidth(300);
+        passfield.setPrefHeight(30);
+        passfield.setPromptText("Please re-enter password");
+        rootPane.getChildren().add(passfield1);
+
+        // Button Settings //
+        Button btn = new Button();
+        btn.setText("Register");
+        btn.setLayoutX(170);
+        btn.setLayoutY(300);
+        btn.setPrefWidth(60);
+        btn.setPrefHeight(30);
+        btn.setOnAction((ActionEvent ae) -> validation());
+        rootPane.getChildren().add(btn);
+    }
+
+    private static void validation() {
+
+    //Check if passwords match
 
 
     }
